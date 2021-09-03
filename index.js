75002d02214eb54866f711dba88fadc6647b8b16
Dany Gagnon <1679863@cstjean.qc.ca>
2021-09-03 12:30:20 -0400

    ajout du script pour les operations

---
+++ index.js
@@ -0,0 +1,53 @@
+const calculateBtn = document.getElementById("calc");
+const operator = document.getElementById("operateur");
+
+const opr = [
+    {
+        operator: "*",
+        operation: (a, b) => a * b,
+    },
+    {
+        operator: "+",
+        operation: (a, b) => a + b,
+    },
+    {
+        operator: "-",
+        operation: (a, b) => a - b,
+    },
+    {
+        operator: "/",
+        operation: (a, b) => a / b,
+    },
+    {
+        operator: "**",
+        operation: (a, b) => a ** b,
+    },
+    {
+        operator: "any",
+        operation: (a, b) => `${a}${b}`,
+    },
+];
+
+function operation(arg) {
+    const filtered = opr.filter((o) => o.operator === arg);
+    return filtered.length === 0 ? opr.filter((o) => o.operator === "any")[0].operation : filtered[0].operation;
+}
+
+operator.onchange = () => {
+    const info = document.getElementById("info");
+    const isInArray = opr.filter((e) => e.operator === operator.value).length > 0;
+
+    info.innerText = isInArray ? "" : `${operator.value} utilise la concaténation.`;
+};
+
+calculateBtn.onclick = (e) => {
+    e.preventDefault();
+    const firstValue = Number(document.getElementById("valeur1").value);
+    const secondValue = Number(document.getElementById("valeur2").value);
+    const operatorValue = document.getElementById("operateur").value;
+    const solution = document.getElementById("solution");
+
+    const results = operation(operatorValue)(firstValue, secondValue);
+
+    solution.setAttribute("value", results);
+};
